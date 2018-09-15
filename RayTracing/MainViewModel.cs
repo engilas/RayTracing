83fee4895d900b3ce2405782cb5542a2202c891a
a.rydaev <shorks@ya.ru>
2018-09-15 23:21:18 +0300

    Scene update

--- RayTracing/MainViewModel.cs
+++ RayTracing/MainViewModel.cs
@@ -66,20 +66,13 @@ namespace RayTracing {
                         Specular = 10,
                         Reflect = 0.4
                     },
-                    new Sphere {
-                        Center = new Vector(0.7, 1.8, -1.7),
-                        Radius = 0.3,
-                        Color = Color.FromRgb(255, 0, 0),
-                        Specular = 500,
-                        Reflect = 0.2
-                    },
-                    new Sphere {
-                        Center = new Vector(0.7, 1.8, 1.7),
-                        Radius = 0.3,
-                        Color = Color.FromRgb(255, 0, 0),
-                        Specular = 500,
-                        Reflect = 0.2
-                    },
+                    //new Sphere {
+                    //    Center = new Vector(0.7, 1.8, 1.7),
+                    //    Radius = 0.6,
+                    //    Color = Color.FromRgb(255, 0, 0),
+                    //    Specular = 500,
+                    //    Reflect = 0.2
+                    //},
                     new Sphere
                     {
                         Color = Color.FromRgb(255, 255, 255),
@@ -121,20 +114,20 @@ namespace RayTracing {
                         A = 0.0,
                         B = 0,
                         C = -1,
-                        D = 7,
+                        D = 15,
                         Reflect = 0.3,
                         Specular = 50
                     }
                 },
-                //Boxes = new List<Box> {
-                //    new Box {
-                //        Color = Color.FromRgb(255, 147, 0),
-                //        Min = new Vector(-20, 1, -20),
-                //        Max = new Vector(20, 1.5, 20),
-                //        Reflect = 0.2,
-                //        Specular = 200
-                //    }
-                //},
+                Boxes = new List<Box> {
+                    new Box {
+                        Color = Color.FromRgb(255, 147, 0),
+                        Min = new Vector(3.5, 0.3, 6),
+                        Max = new Vector(4.5, 3.5, 7),
+                        Reflect = 0.2,
+                        Specular = 200
+                    }
+                },
                 Surfaces = new List<Surface>
                 {
                     new Surface
@@ -146,7 +139,7 @@ namespace RayTracing {
                         Direction = Direction.Down,
                         Width = 1,
                         //Edge = 0.5,
-                        Offset = new Vector(-2, 8, 0)
+                        Offset = new Vector(-2, 7, 10)
                     }
                 }
             };
@@ -156,7 +149,7 @@ namespace RayTracing {
                 //CameraPos = new Vector(-2, 0, -2),
                 //CameraPos = new Vector(1, 5, 0),
                 //CameraPos = new Vector(5, 0, 0),
-			    CameraPos = new Vector(-9, 0, 2),
+			    CameraPos = new Vector(0, 1, -7),
                 ViewportWidth = 1,
 				ViewportHeight = 1,
 				CanvasWidth = Width,
@@ -164,7 +157,7 @@ namespace RayTracing {
 				ViewportDistance = 1,
 				RecursionDepth = 3,
 				//CameraRotationX = -45,
-				CameraRotationY = -90
+				//CameraRotationY = -180
 			};
 
 			Console.WriteLine("Started");
